39231828c222b5e3ab35d873503f4b502a88049a
redfuegolight <xxxredfuegolight@gmail.com>
2016-01-02 16:19:00 +0100

    aggiunti file

---
+++ italiano/thankyoulike.lang.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * Thank You / Like System Italian Language Pack
+ * Powered By redfuegolight
+ * https://www.mybb-it.com/User-redfuegolight
+ */
+
+$l['tyl_thankyou'] = "Grazie";
+$l['tyl_thanked'] = "Ha ringraziato";
+$l['tyl_thanks'] = "Grazie";
+$l['tyl_thanks_given'] = "Grazie dati";
+$l['tyl_thanks_rcvd'] = "Grazie ricevuti";
+$l['tyl_thanks_rcvd_bit'] = "{1} in {2} messaggi";
+$l['tyl_like'] = "Mi piace";
+$l['tyl_likes'] = "Mi piace";
+$l['tyl_likes_given'] = "Mi piace dati";
+$l['tyl_likes_rcvd'] = "Mi piace ricevuti";
+$l['tyl_likes_rcvd_bit'] = "{1} in {2} messaggi";
+$l['tyl_liked'] = "Piaciuto";
+
+$l['tyl_users'] = "utenti";
+$l['tyl_user'] = "utente";
+$l['tyl_says'] = "dicono";
+$l['tyl_say'] = "dice";
+
+$l['tyl_title_ty'] = "<strong>I seguenti {1} {2} ringraziano {4} per questo messaggio:</strong>";
+$l['tyl_title_l'] = "<strong>I seguenti {1} {2} {3} {4} messaggi:</strong>";
+$l['tyl_title_collapsed_ty'] = "<strong>{1} {2} ringraziano {4} per questo messaggio</strong>";
+$l['tyl_title_collapsed_l'] = "<strong>{1} {2} {3} {4} messaggi</strong>";
+
+$l['add_ty'] = "Ringrazia";
+$l['del_ty'] = "Rimuovi grazie";
+$l['add_l'] = "Mi piace";
+$l['del_l'] = "Non mi piace";
+
+$l['tyl_total_thanks_given'] = "Totale grazie dati";
+$l['tyl_total_likes_given'] = "Totale di mi piace dati";
+$l['tyl_total_thanks_rcvd'] = "Totale di grazie ricevuti";
+$l['tyl_total_likes_rcvd'] = "Totale di mi piace ricevuti";
+$l['tyl_tylpd_percent_total'] = "{1} {2} per giorno | {3} percento dei grazie totali";
+$l['tyl_find_ty_threads'] = "Tutti gli argomenti ringraziati";
+$l['tyl_find_ty_posts'] = "Trova TUTTI i messaggi in cui è ringraziato";
+$l['tyl_find_l_threads'] = "Trova tutti gli argomenti in cui c'è il mi piace";
+$l['tyl_find_l_posts'] = "Trova tutti i messaggi in cui c'è il mi piace";
+$l['tyl_find_ty_threads_for'] = "Trova tutti gli argomenti ringraziati per";
+$l['tyl_find_ty_posts_for'] = "Trova tutti i messaggi ringraziati per";
+$l['tyl_find_l_threads_for'] = "Trova tutti gli argomenti con il mi piace per";
+$l['tyl_find_l_posts_for'] = "Trova tutti i messaggi con il mi piace per";
+
+$l['tyl_wol_searching'] = "<a href=\"{1}\">Ricerca {2}</a>";
+
+$l['tyl_error_invalid_action'] = "Stai tentando di eseguire un'azione non valida.";
+$l['tyl_error_disabled'] = "{1} La funzione è stata disabilitata.";
+$l['tyl_error_not_allowed'] = "Non consentito per questo messaggio";
+$l['tyl_error_excluded'] = "Non consentito per questo messaggio perchè questa categoria è stata esculsa.";
+$l['tyl_error_own_post'] = "Non è possibile {1} il proprio messaggio.";
+$l['tyl_error_already_tyled'] = "Hai già {1} questo messaggio.";
+$l['tyl_error_unknown'] = "Si è verificato un errore sconosciuto e il tuo {1} non è stato aggiunto.";
+$l['tyl_error_removal_disabled'] = "Non è possibile rimuovere il proprio {1} perchè la rimozione è stata disattivata.";
+$l['tyl_error_own_delete'] = "Questo {1} non può essere rimosso, perchè non sei l'utente che ha pubblicato questo {1}. E' possibile eliminare solo {1} che è stato aggiunto ai messaggi.";
+$l['tyl_error_not_found'] = "{1} non può essere rimosso, perchè non è stato possibile trovarlo. Potrebbe essere stato già rimosso.";
+$l['tyl_error_threadclosed'] = "Non è possibile {1} questo messaggi perchè è stato chiuso.";
+
+$l['tyl_redirect_tyled'] = "Il tuo {1} è stato aggiunto al messaggio";
+$l['tyl_redirect_deleted'] = "il tuo {1} è stato rimosso dal messaggio";
+$l['tyl_redirect_back'] = "<br />Ora verrai riportato al messaggio.";
+
+
+$l['tyl_alert'] = '{1} ringraziato/messo mi piace al tuo messaggio <b>"{2}"</b>.';
+$l['tyl_alert_like'] = '{1} ha messo mi piace al tuo messaggio <b>"{2}"</b>.';
+$l['tyl_alert_thanks'] = '{1} ha ringraziato il tuo messaggio <b>"{2}"</b>.';
+$l['myalerts_setting_tyl'] = 'Vuoi ricevere un avviso quando qualcuno aggiunge grazie/mi piace ai tuoi messaggio?';
+$l['myalerts_setting_tyl_like'] = 'Vuoi ricevere un avviso quando qualcuno aggiunge mi piace ai tuoi messaggi?';
+$l['myalerts_setting_tyl_thanks'] = 'Vuoi ricevere un avviso quando qualcuno aggiunge grazie ai tuoi messaggi?';
+
+$l['tyl_send_like'] = 'Sto aggiungendo mi piace a questo messaggio';
+$l['tyl_send_thanks'] = 'Sto aggiungendo grazie a questo messaggio';
+$l['tyl_remove_like'] = 'Sto rimuovendo il mi piace da questo messaggio';
+$l['tyl_remove_thanks'] = 'Sto rimuovendo il grazie da questo messaggio';
